ef1c27f6dc7531beb40b5e00e3b691a7d5e5f08c
mmmim24 <mustaq24@student.sust.edu>
2025-03-29 23:49:56 +0600

    useMemo to cache

--- src/counties/Countries.jsx
+++ src/counties/Countries.jsx
@@ -1,44 +1,44 @@
-import { Suspense, useState } from "react";
-import Country from "./country";
+import { Suspense, useState, useMemo } from "react";
+import Country from "./Country";
 import "./css/country.css"
 
 export default function Countries() {
 
-    const fetchCountries = async() => {
-       const fetchData = await fetch('https://restcountries.com/v3.1/all')
-       console.log('test')
-       return fetchData.json();
-       
-    }
-    
-    const fetchData = fetchCountries();
- 
-   
-    const [country, setCountry] =useState([]);
+  const fetchCountries = async () => {
+    const fetchData = await fetch('https://restcountries.com/v3.1/all')
+    console.log('test')
+    return fetchData.json();
 
-   function visitedCountrySoFar(visitedCountry,toggle){
+  }
+
+  const fetchData = useMemo(() => fetchCountries(), []);
+
+
+  const [country, setCountry] = useState([]);
+
+  function visitedCountrySoFar(visitedCountry, toggle) {
     setCountry(visitedCountry);
-   }
-   
-   
-   console.log(country)
+  }
 
-   return (
-        <>
-        
-          <h1>Visited Country :</h1> 
-          { 
-            
-            country.map((visited , i) => <span key={i}>{visited} ,</span>)
 
-            }   
-        <div className="country-list">
-        
+  console.log(country)
+
+  return (
+    <>
+
+      <h1>Visited Country :</h1>
+      {
+
+        country.map((visited, i) => <span key={i}>{visited} ,</span>)
+
+      }
+      <div className="country-list">
+
         <Suspense fallback="Loading....">
-         <Country fetchData={fetchData} visitedCountrySoFar={visitedCountrySoFar}></Country>
+          <Country fetchData={fetchData} visitedCountrySoFar={visitedCountrySoFar}></Country>
         </Suspense>
-        </div>
-        </>
+      </div>
+    </>
 
-    )
+  )
 }
