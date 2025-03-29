ca3e43f1bda6c442f46fab74665c9df9a812c0d7
mmmim24 <mustaq24@student.sust.edu>
2025-03-30 00:16:58 +0600

    props passing undone

--- src/counties/Countries.jsx
+++ src/counties/Countries.jsx
@@ -1,44 +1,29 @@
 import { Suspense, useState } from "react";
-import Country from "./country";
+import Country from "./Country";
 import "./css/country.css"
 
 export default function Countries() {
 
-    const fetchCountries = async() => {
-       const fetchData = await fetch('https://restcountries.com/v3.1/all')
-       console.log('test')
-       return fetchData.json();
-       
-    }
-    
-    const fetchData = fetchCountries();
- 
-   
-    const [country, setCountry] =useState([]);
+  const fetchCountries = async () => {
+    const fetchData = await fetch('https://restcountries.com/v3.1/all')
+    console.log('test')
+    return fetchData.json();
 
-   function visitedCountrySoFar(visitedCountry,toggle){
-    setCountry(visitedCountry);
-   }
-   
-   
-   console.log(country)
+  }
 
-   return (
-        <>
-        
-          <h1>Visited Country :</h1> 
-          { 
-            
-            country.map((visited , i) => <span key={i}>{visited} ,</span>)
+  const fetchData = fetchCountries();
 
-            }   
-        <div className="country-list">
-        
+  return (
+    <>
+
+
+      <div className="country-list">
+
         <Suspense fallback="Loading....">
-         <Country fetchData={fetchData} visitedCountrySoFar={visitedCountrySoFar}></Country>
+          <Country fetchData={fetchData} ></Country>
         </Suspense>
-        </div>
-        </>
+      </div>
+    </>
 
-    )
+  )
 }
--- src/counties/Country.jsx
+++ src/counties/Country.jsx
@@ -5,25 +5,33 @@ import { Data } from "./Data";
 
 
 
-export default function Country({ fetchData ,visitedCountrySoFar}) {
-    
+export default function Country({ fetchData, visitedCountrySoFar }) {
 
-    const [visited , setVisited] = useState([])
+
+    const [visited, setVisited] = useState([])
     const visitedCountry = (name) => {
-      
-        const newVisited = [...visited , name]
+
+        const newVisited = [...visited, name]
         setVisited(newVisited);
 
         visitedCountrySoFar(newVisited)
         // console.log(newVisited)
-        
+
     }
-    
+
     const actualData = use(fetchData);
 
     return (
         <>
+            <div>
+                <h1>Visited Country :</h1>
 
+                {
+
+                    visited.map((visited, i) => <span key={i}>{visited} ,</span>)
+
+                }
+            </div>
             {
                 actualData.map((single, i) => <Data key={i} visitedCountry={visitedCountry} singleData={single} ></Data>)
             }
